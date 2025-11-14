a11f278abbd622ae4367a3ae8d47438c9340be61
Muhamad Fudhail <136212616+MuhammadFudhail@users.noreply.github.com>
2025-11-14 10:00:02 +0800

    menambahkan url deploy be

--- Task-Manager/src/utils/apiPaths.js
+++ Task-Manager/src/utils/apiPaths.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:8000";
+// export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://project-akhir-be-production.up.railway.app";
 
 //utils/apipath.js
 export const API_PATHS = {
