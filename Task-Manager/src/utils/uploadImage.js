a91d7205bf2e906c6fa099dae48a90aa035d68ff
Muhamad Fudhail <136212616+MuhammadFudhail@users.noreply.github.com>
2025-11-17 13:26:58 +0800

    fixx uplagi

--- Task-Manager/src/utils/uploadImage.js
+++ Task-Manager/src/utils/uploadImage.js
@@ -1,22 +1,29 @@
-import { API_PATHS } from "./apiPaths";
-import axiosInstance from "./axiosInstance";
-
 const uploadImage = async (imageFile) => {
-    const formData = new FormData()
+  const formData = new FormData();
 
-    formData.append('image', imageFile);
+  // Field yang wajib untuk Cloudinary
+  formData.append("file", imageFile);
+  formData.append("upload_preset", "myapp_upload"); // ganti dengan nama preset kamu
 
-    try {
-        const response = await axiosInstance.post(API_PATHS.IMAGE.UPLOAD_IMAGE, formData, {
-            headers: {
-                'Content-Type': 'multipart/form-data',
-            },
-        })
-        return response.data;
-    } catch (error) {
-        console.error('Error uploading the image', error)
-        throw error; 
+  try {
+    const res = await fetch(
+      "https://api.cloudinary.com/v1_1/djs1k1meh/image/upload",
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+
+    if (!res.ok) {
+      throw new Error("Upload failed");
     }
-}
+
+    const data = await res.json();
+    return data.secure_url; // ini URL final gambarnya
+  } catch (error) {
+    console.error("Error uploading image:", error);
+    throw error;
+  }
+};
 
 export default uploadImage;
