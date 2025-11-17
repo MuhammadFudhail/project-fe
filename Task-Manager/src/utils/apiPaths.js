76acc25bf73f3c783fc5cb2434d30b9607ba4c51
Muhamad Fudhail <136212616+MuhammadFudhail@users.noreply.github.com>
2025-11-17 11:18:16 +0800

    fixx bug upload

--- Task-Manager/src/utils/apiPaths.js
+++ Task-Manager/src/utils/apiPaths.js
@@ -36,6 +36,6 @@ export const API_PATHS = {
 
   IMAGE: {
     // UPLOAD_IMAGE: "/api/auth/upload-image",
-    UPLOAD_IMAGE: "/api/upload/profile",
+    UPLOAD_IMAGE: "/api/profile/upload",
   },
 };
